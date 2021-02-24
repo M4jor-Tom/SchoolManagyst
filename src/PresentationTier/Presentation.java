36c65b19be229c6e40dd4b6efa21a4d6ec575123
Tom <tom-vautray@hotmail.fr>
2021-02-24 23:39:08 +0100

    Menu management set into Presentation Tier

--- src/PresentationTier/Presentation.java
+++ src/PresentationTier/Presentation.java
@@ -1,5 +1,6 @@
 package PresentationTier;
 import LogicTier.LogicInterface;
+import MenuPackage.Menu;
 
 public class Presentation
 {
@@ -8,5 +9,23 @@ public class Presentation
 	public Presentation(LogicInterface logicInterface)
 	{
 		_logicInterface = logicInterface;
+	}
+	
+	public void menu()
+	{
+		Menu mainMenu = new Menu();
+		mainMenu.addChoice("Afficher les acronymes de chaque promotion");
+		mainMenu.addChoice("Rechercher une promotion par son acronyme");
+		mainMenu.addChoice("Ajouter une nouvelle promotion");
+		mainMenu.addChoice("Créer, puis inscrire un étudiant à une promotion");
+		mainMenu.addChoice("Voir tous les étudiants de l'école");
+		mainMenu.addExit("Quitter");
+		while(!mainMenu.leaving())
+		{
+			switch(mainMenu.display("Mais... que faire ?\n"))
+			{
+			
+			}
+		}
 	}
 }
