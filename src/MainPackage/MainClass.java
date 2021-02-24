4d674ddb929861a33f58440db6aa8a5e6678f6f2
Tom <tom-vautray@hotmail.fr>
2021-02-24 22:52:49 +0100

    Menu instanciation in MainPackage

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -12,6 +12,14 @@ public class MainClass
 {
 	public static void main(String[] args)
 	{
-		
+		Menu mainMenu = new Menu();
+		mainMenu.addChoice("Afficher les acronymes de chaque promotion");
+		mainMenu.addChoice("Rechercher une promotion par son acronyme");
+		mainMenu.addChoice("Ajouter une nouvelle promotion");
+		mainMenu.addChoice("Créer, puis inscrire un étudiant à une promotion");
+		mainMenu.addChoice("Voir tous les étudiants de l'école");
+		mainMenu.addExit("Quitter");
+		while(!mainMenu.leaving())
+			mainMenu.display("Mais... que faire ?\n");
 	}
 }
