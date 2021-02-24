9390cbe2a1e6e89cacb09bb5d0421058de56c7a2
Tom <tom-vautray@hotmail.fr>
2021-02-24 22:30:41 +0100

    3-Tier Architecture implementation in MainPackage

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -1,4 +1,11 @@
 package MainPackage;
+
+//3-Tier Implementation
+import PresentationTier.Presentation;
+import LogicTier.LogicInterface;
+import DataTier.DataInterface;
+
+//Menu implementation
 import MenuPackage.Menu;
 
 public class MainClass
