66ea62aceeed1177f66fb49454863de2abac5c21
Tom <tom-vautray@hotmail.fr>
2021-02-24 23:43:35 +0100

    Menu removal from MainClass

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -1,25 +1,14 @@
 package MainPackage;
 
 //3-Tier Implementation
-import PresentationTier.Presentation;
-import LogicTier.LogicInterface;
-import DataTier.DataInterface;
-
-//Menu implementation
-import MenuPackage.Menu;
+import PresentationTier.*;
+import LogicTier.*;
+import DataTier.*;
 
 public class MainClass
 {
 	public static void main(String[] args)
 	{
-		Menu mainMenu = new Menu();
-		mainMenu.addChoice("Afficher les acronymes de chaque promotion");
-		mainMenu.addChoice("Rechercher une promotion par son acronyme");
-		mainMenu.addChoice("Ajouter une nouvelle promotion");
-		mainMenu.addChoice("Créer, puis inscrire un étudiant à une promotion");
-		mainMenu.addChoice("Voir tous les étudiants de l'école");
-		mainMenu.addExit("Quitter");
-		while(!mainMenu.leaving())
-			mainMenu.display("Mais... que faire ?\n");
+
 	}
 }
