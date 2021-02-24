0e5211781bd73d4317118ee2737820142166bab0
Tom <tom-vautray@hotmail.fr>
2021-02-24 22:19:16 +0100

    Connected each tiers of 3-Tier architecture

--- src/LogicTier/Logic.java
+++ src/LogicTier/Logic.java
@@ -3,5 +3,10 @@ import DataTier.DataInterface;
 
 public class Logic implements LogicInterface
 {
+	private DataInterface _dataInterface;
 	
+	public Logic(DataInterface dataInterface)
+	{
+		_dataInterface = dataInterface;
+	}
 }
--- src/PresentationTier/Presentation.java
+++ src/PresentationTier/Presentation.java
@@ -3,5 +3,10 @@ import LogicTier.LogicInterface;
 
 public class Presentation
 {
+	private LogicInterface _logicInterface;
 	
+	public Presentation(LogicInterface logicInterface)
+	{
+		_logicInterface = logicInterface;
+	}
 }
