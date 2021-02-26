336da4b77546f9f3bc70582e55c7e90edde7ea28
Tom <tom-vautray@hotmail.fr>
2021-02-26 11:27:36 +0100

    Presentation: Get Promotion by acronym

--- src/PresentationTier/Presentation.java
+++ src/PresentationTier/Presentation.java
@@ -1,14 +1,20 @@
 package PresentationTier;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import LogicTier.LogicInterface;
 import MenuPackage.Menu;
+import MainPackage.Promotion;
 
 public class Presentation
 {
 	private LogicInterface _logicInterface;
+	private Scanner _scanner;
 	
 	public Presentation(LogicInterface logicInterface)
 	{
 		_logicInterface = logicInterface;
+		_scanner = new Scanner(System.in);
 	}
 	
 	public void menu()
@@ -24,7 +30,17 @@ public class Presentation
 		{
 			switch(mainMenu.display("Mais... que faire ?\n"))
 			{
-			
+			case 0:
+				break;
+				
+			case 1:
+				System.out.print("Acronyme: ");
+				Promotion found = _logicInterface.getPromotion(_scanner.next());
+				if(found.isNull())
+					System.out.println("Aucune promotion trouvée");
+				else
+					System.out.println(found);
+				break;
 			}
 		}
 	}
