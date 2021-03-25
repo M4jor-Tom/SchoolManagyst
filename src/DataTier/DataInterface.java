c93a85521dbdaa74bfb6554f1ef96467d0ca5274
Tom <tom-vautray@hotmail.fr>
2021-03-25 15:26:52 +0100

    Added getPromotions() and setPromotions() in DataInterface

--- src/DataTier/DataInterface.java
+++ src/DataTier/DataInterface.java
@@ -1,6 +1,11 @@
 package DataTier;
 
+import java.util.ArrayList;
+
+import MainPackage.Promotion;
+
 public interface DataInterface
 {
-
+	ArrayList<Promotion> getPromotions();
+	void setPromotions(ArrayList<Promotion> promotions);
 }
