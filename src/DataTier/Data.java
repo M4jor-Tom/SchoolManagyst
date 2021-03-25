e01a1db4e1485aa338709b6c361065bf349c5ce9
Tom <tom-vautray@hotmail.fr>
2021-03-25 15:07:33 +0100

    Added DAOs to Data tier with its Studients / Promotions ArrayLists

--- src/DataTier/Data.java
+++ src/DataTier/Data.java
@@ -1,6 +1,38 @@
 package DataTier;
 
+import java.util.ArrayList;
+
+import MainPackage.Promotion;
+import MainPackage.Studient;
+
 public class Data implements DataInterface
 {
-
+	ArrayList<Promotion> _promotions;
+	ArrayList<Studient> _studients;
+	
+	public Data(PromotionDataAccessObject promotionDao)
+	{
+		setPromotions(new ArrayList<>());
+		setStudients(new ArrayList<>());
+	}
+	
+	public ArrayList<Promotion> getPromotions()
+	{
+		return _promotions;
+	}
+	
+	public void setPromotions(ArrayList<Promotion> promotions)
+	{
+		_promotions = promotions;
+	}
+	
+	public ArrayList<Studient> getStudients()
+	{
+		return _studients;
+	}
+	
+	public void setStudients(ArrayList<Studient> studients)
+	{
+		_studients = studients;
+	}
 }
