a70c2b12ccb17c6a1c4aac95b4ff15f2639176c1
Tom <tom-vautray@hotmail.fr>
2021-03-25 15:07:00 +0100

    Added PromotionDataAccessObject extends DataAccessObject
    already owns create() method

---
+++ src/DataTier/PromotionDataAccessObject.java
@@ -0,0 +1,63 @@
+package DataTier;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import MainPackage.Promotion;
+
+public class PromotionDataAccessObject extends DataAccessObject<Promotion>
+{	
+	public PromotionDataAccessObject(StudientDataAccessObject studientDao)
+	{
+		super();
+		
+	}
+
+	public void create(Promotion promotion) {
+		try
+		{	
+			PreparedStatement prepare = getConnection().prepareStatement(
+				"INSERT INTO promotion (promotionEntitled, promotionAcronym) VALUES (?, ?)"
+			);
+			
+			prepare.setString(1, promotion.getEntitled());
+			prepare.setString(2, promotion.getAcronym());
+			prepare.executeUpdate();
+			prepare.close();
+		}
+		catch
+		(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void delete(Promotion object)
+	{
+		
+		
+	}
+
+	@Override
+	public void update(Promotion object)
+	{
+		
+		
+	}
+
+	@Override
+	public Promotion select(Long id)
+	{
+		
+		return null;
+	}
+
+	@Override
+	public ArrayList<Promotion> selectAll()
+	{
+		
+		return null;
+	}
+}
